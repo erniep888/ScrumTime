e17e08774b9ad75e19088da683a203653baf9b1b
Ernie Paschall <epaschall@scrumtime.org>
2010-07-14 12:48:26 -0400

    Found alizarin

---
+++ Alizarin/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScrumTime.Models;
+using ScrumTime.Helpers;
+
+namespace ScrumTime.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Story(string test)
+        {
+            var eval = test;
+            ScrumTimeEntities entities = new ScrumTimeEntities();
+            Story story = entities.Stories.First<Story>();
+            return PartialView("StoryTrControl",story);
+        }
+
+        public ActionResult StoryJson(string test)
+        {
+            string[] acceptTypes = Request.AcceptTypes;
+            
+            var eval = test;
+            ScrumTimeEntities entities = new ScrumTimeEntities();
+            Story story = entities.Stories.First<Story>();
+            if (acceptTypes.Any(v => v.Contains("json")))
+            {
+                List<object> stories = new List<object>();
+                stories.Add(new { Narrative = story.Narrative, Points = story.Points, StoryId = story.StoryId });
+                return new SecureJsonResult(stories);
+            }
+            else
+                return PartialView("StoryControl", story);
+        }
+
+        public ActionResult StoryEdit()
+        {
+            ScrumTimeEntities entities = new ScrumTimeEntities();
+            return PartialView("StoryEditControl");
+        }
+
+    }
+}
---
+++ Alizarin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Alizarin.MvcApplication" Language="C#" %>
