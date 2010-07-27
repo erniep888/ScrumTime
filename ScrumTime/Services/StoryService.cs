3b320a4f4edaf7fd864d041635004e3ba778dcbf
Ernie Paschall <epaschall@scrumtime.org>
2010-07-27 16:43:00 -0400

    Working on the priority changes for stories in the backlog

--- ScrumTime/Services/StoryService.cs
+++ ScrumTime/Services/StoryService.cs
@@ -27,5 +27,73 @@ namespace ScrumTime.Services
                 story = new Story();
             return story;
         }
+
+        public Story SaveStory(Story story)
+        {
+            if (story != null)
+            {
+                Story previousStory = null;
+                int totalNumberOfStories = _ScrumTimeEntities.Stories.Count();
+                if (story.StoryId != null)
+                {
+                    previousStory = GetStoryById(story.StoryId);
+                    if (previousStory != null &&
+                        previousStory.Priority == story.Priority)
+                    {
+                        // No need to update other story priorities
+                    }
+                    else
+                    {
+                        // Update other story priorities greater than this story priority
+
+                    }
+                }
+            }
+            return story;
+        }
+
+        protected void UpdateStoryPriorities(int currentPriority, int totalNumberOfStories)
+        {
+            if (currentPriority < 1)
+                currentPriority = 1;
+            else if (currentPriority > totalNumberOfStories)
+                currentPriority = totalNumberOfStories;
+            var results = from s in _ScrumTimeEntities.Stories
+                          where s.Priority >= currentPriority
+                          orderby s.Priority ascending
+                          select s;
+            foreach (Story story in results)
+            {
+                story.Priority++;
+            }
+            _ScrumTimeEntities.SaveChanges();
+        }
+
+        protected void DecreasePriorityValuesByOne(int start, int stop)
+        {
+            var results = from s in _ScrumTimeEntities.Stories
+                          where s.Priority > start && s.Priority <= stop
+                          orderby s.Priority ascending
+                          select s;
+            foreach (Story s in results)
+            {
+                s.Priority--;
+            }
+            _ScrumTimeEntities.SaveChanges();
+        }
+
+        protected void IncreasePriorityValuesByOne(int start, int stop)
+        {
+            var results = from s in _ScrumTimeEntities.Stories
+                          where s.Priority >= start && s.Priority < stop
+                          orderby s.Priority ascending
+                          select s;
+            foreach (Story s in results)
+            {
+                s.Priority++;
+            }
+            _ScrumTimeEntities.SaveChanges();
+        }
+
     }
 }
