b211d8ee13cd95581c2192d4503482d5423ce828
Ernie Paschall <epaschall@scrumtime.org>
2010-08-20 18:00:56 -0400

    Beginning to add schedule

---
+++ ScrumTime/Controllers/ScheduleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScrumTime.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        //
+        // GET: /Schedule/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
