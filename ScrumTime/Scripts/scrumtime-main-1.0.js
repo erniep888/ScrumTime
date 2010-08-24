4e6730f987fff4bdbc476cc3b8a4193c9fbd2442
Ernie Paschall <epaschall@scrumtime.org>
2010-08-23 21:23:35 -0400

    Added the beginning of the Release schedule screen.

--- ScrumTime/Scripts/scrumtime-main-1.0.js
+++ ScrumTime/Scripts/scrumtime-main-1.0.js
@@ -1,11 +1,71 @@
 ﻿$(function () {
     $("#mainMenu").tabs();
-    $("#mainMenu").tabs("option", "cache", true);
-    $("#mainMenu").tabs("option", "ajaxOptions", { cache: true });
+    $("#mainMenu").tabs("option", "cache", false);
+    $("#mainMenu").tabs("option", "ajaxOptions", { cache: false });
 });
 
 function loadBacklogTab() {
     $(function () {
         $("#mainMenu").tabs('select', 2);
     });
+}
+
+
+function loadSchedule() {
+    $(function () {
+        var d = [[1196463600000, 2], [1198980000000, 2]];
+        var d2 = [[1198980000000, 1], [1200550000000, 1]];
+
+        // first correct the timestamps - they are recorded as the daily
+        // midnights in UTC+0100, but Flot always displays dates in UTC
+        // so we have to add one hour to hit the midnights in the plot
+        for (var i = 0; i < d.length; ++i)
+            d[i][0] += 60 * 60 * 1000;
+
+        // helper for returning the weekends in a period
+        function weekendAreas(axes) {
+            var markings = [];
+            var d = new Date(axes.xaxis.min);
+            // go to the first Saturday
+            d.setUTCDate(d.getUTCDate() - ((d.getUTCDay() + 1) % 7))
+            d.setUTCSeconds(0);
+            d.setUTCMinutes(0);
+            d.setUTCHours(0);
+            var i = d.getTime();
+            do {
+                // when we don't set yaxis, the rectangle automatically
+                // extends to infinity upwards and downwards
+                markings.push({ xaxis: { from: i, to: i + 2 * 24 * 60 * 60 * 1000 }, color: "#ddd" });
+                i += 7 * 24 * 60 * 60 * 1000;
+            } while (i < axes.xaxis.max);
+
+            return markings;
+        }
+
+        var options = {
+            xaxis: { mode: "time", min: 1196393600000, max: 1200630000000, timeformat: "%m/%d/%y", tickSize: [7, "day"] },
+            yaxis: { min: 0, max: 3, ticks: [], autoscaleMargin: 0.1 },
+            grid: { markings: weekendAreas, clickable: true, borderColor: "#999" },
+            series: {
+                lines: { show: true, lineWidth: 14 },
+                points: { show: true},
+                shadowSize: 3
+            },
+            legend: { show: true }
+        };
+
+        var releaseSchedule = $("#releaseSchedule");
+
+        $("#releaseSchedule").bind("plotclick", function (event, pos, item) {
+            if (item) {
+                $("#clickdata").text("You clicked point " + item.dataIndex + " in " + item.series.label + ".");
+                plot.highlight(item.series, item.datapoint);
+            }
+        });
+
+        var plot = $.plot($("#releaseSchedule"), [{ label: "Release 0.6", data: d }, { label: "Release 0.9", data: d2}], options);
+
+        //yaxis: { ticks: [], min: 0, autoscaleMargin: 0.1 },        
+    });
+
 }
--- ScrumTime/Views/Schedule/Index.ascx
+++ ScrumTime/Views/Schedule/Index.ascx
@@ -1,2 +1,22 @@
 ﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl<dynamic>" %>
 
+<script type="text/javascript">
+    $(document).ready(function () {
+        document.title = 'ScrumTime 0.9 - Schedule';
+    }); 
+</script> 
+
+<div class="nopadding" style="border:2px solid #999;height:34px;width:947px;margin-left:6px;
+    margin-bottom:-8px;text-align:center;font-family:Verdana;font-size:12px;">
+    <div style="position:absolute;left:10px;width:115px;margin-top:10px">
+        <a href="#" class="simpleLink newStoryLink" onclick="addStoryRow();">Add Story</a>
+    </div>   
+    <div class="nopadding" style="color:#79a52c;font-weight:bold;letter-spacing:1px;font-size:13px;margin-top:3px;">Releases</div>
+</div>
+<div id="releaseSchedule" style="width:963px;height:200px;"></div>
+
+<span id="clickdata"></span>
+
+<script type="text/javascript">
+    loadSchedule();
+</script>
