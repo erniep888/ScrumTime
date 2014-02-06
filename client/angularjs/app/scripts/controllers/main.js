a8288c14ce31591bb0accdb2e1d41613f9445067
Ernie Paschall <erniep888@yahoo.com>
2014-02-06 17:12:09 -0500

    Establishing the scrum time source repository structure that allows for a separation of client source from the backend server source.

---
+++ client/angularjs/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('scrumTimeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ server/database/mongodb/README.txt
@@ -0,0 +1,1 @@
+The default mongo db will go here.  Once it has been checked in, this file will go away.
