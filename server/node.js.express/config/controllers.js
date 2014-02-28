146452d51b7a8ab646b61842e3d6ca338eab704f
Ernie Paschall <erniep888@yahoo.com>
2014-02-27 19:30:28 -0500

    Taking a more modular approach to the Express implementation.

---
+++ server/node.js.express/config/controllers.js
@@ -0,0 +1,10 @@
+/**
+ * Load controllers.
+ */
+
+var homeController = require('./controllers/home');
+var userController = require('./controllers/user');
+var apiController = require('./controllers/api');
+var contactController = require('./controllers/contact');
+var forgotController = require('./controllers/forgot');
+var resetController = require('./controllers/reset');
