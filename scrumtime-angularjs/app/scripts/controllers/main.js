e4400a0f9e1d1cc90aee1fca5cead1751279ebdd
Ernie Paschall <erniep888@yahoo.com>
2014-03-02 18:00:16 -0500

    Separating the express project from the angularjs project.

---
+++ scrumtime-angularjs/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('scrumtimeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
