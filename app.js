61d0f9a2752db24180ca111e69417fb6cc7b13e4
Ernie Paschall <erniep888@yahoo.com>
2014-03-01 19:21:00 -0500

    A few updates in preparation to get an initial running Express app.

--- app.js
+++ app.js
@@ -1,41 +1,34 @@
-/*jshint node:true*/
+
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 
 var app = express();
-var server = http.createServer(app);
-var io = require('socket.io').listen(server);
 
-app.set('port', process.env.VCAP_APP_PORT || 3000);
+// all environments
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Handle Errors gracefully
-app.use(function(err, req, res, next) {
-	if(!err) return next();
-	console.log(err.stack);
-	res.json({error: true});
-});
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
-// Main App Page
 app.get('/', routes.index);
 
-// MongoDB API Routes
-app.get('/polls/polls', routes.list);
-app.get('/polls/:id', routes.poll);
-app.post('/polls', routes.create);
-app.post('/vote', routes.vote);
-
-io.sockets.on('connection', routes.vote);
-
-server.listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
